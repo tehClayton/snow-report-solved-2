97450fbe8fbe7ad2c5462971f6b81055bc9ba630
Clayton Noyes <clayton.noyes@gmail.com>
2018-11-09 16:09:37 -0700

    Add scrape endpoint and call page scraper

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect
+import scraper
 
 app = Flask(__name__)
 
@@ -8,6 +9,15 @@ def home():
 	# Return template and data
 	return render_template("index.html")
 
+
+@app.route("/scrape")
+def scrape():
+	scraped_resorts = scraper.scrape_page()
+	
+	print(scraped_resorts)
+
+	# Return template and data
+	return redirect('/')
 
 
 if __name__ == "__main__":
